a9f6973615d72566cb442fae954859566b27a7e3
krakenivan <krakenivan777@gmail.com>
2025-03-19 06:52:14 +0400

    using services for find_order_app

--- order_management/edit_order_app/forms.py
+++ order_management/edit_order_app/forms.py
@@ -1,24 +1,25 @@
 from django import forms
 from django.utils.safestring import mark_safe
+
 from common.models import Order
 from common.services.dishes_services import current_dishes_of_order, get_id_product_of_dish
 from common.services.product_services import exclude_product
 
 
 class CustomTextWidget(forms.TextInput):
+    """виджет для формы для отображения текста"""
     def __init__(self, custom_text=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.custom_text = custom_text  # Сохраняем переданный текст
 
     def render(self, name, value, attrs=None, renderer=None):
-        # Рендерим стандартное поле ввода
         super().render(name, value, attrs, renderer)
         # Добавляем кастомный текст перед полем
         return mark_safe(f"<div class='info' >{self.custom_text}</div>")
 
 
 class EditOrderForm(forms.ModelForm):
-
+    """Форма изменения заказа"""
     class Meta:
         model = Order
         exclude = ["total_price"]
@@ -29,21 +30,17 @@ class EditOrderForm(forms.ModelForm):
         if "instance" not in kwargs:
             return
         order = kwargs["instance"]
-        # dishes = order.dishes_set.all()
         dishes = current_dishes_of_order(order)
-        # products = Product.objects.exclude(id__in=dishes.values("product_id"))
         products = exclude_product(id__in=dishes.values("product_id"))
 
         self.fields["dishes_in_order"] = forms.CharField(
             required=False,
             label="",
             widget=CustomTextWidget(custom_text="Блюда в заказе:"),
-        )
+        )  # поле для отображения инфо текста
         # формы для блюд в заказе
         for dish in dishes:
-            # field_name = f"product_{dish.product.id}"
             field_name = f"product_{get_id_product_of_dish(dish)}"
-            # quantity_name = f"quantity_{dish.product.id}"
             quantity_name = f"quantity_{get_id_product_of_dish(dish)}"
 
             # Поле для выбора продукта (чекбокс)
@@ -65,7 +62,7 @@ class EditOrderForm(forms.ModelForm):
             required=False,
             label="",
             widget=CustomTextWidget(custom_text="Добавить блюда:"),
-        )
+        )  # поле для отображения инфо текста
         # формы для блюд вне заказа
         for product in products:
             field_name = f"product_{product.id}"
--- order_management/find_order_app/views.py
+++ order_management/find_order_app/views.py
@@ -1,36 +1,41 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from common.models import Order
 from django.views.generic import FormView
+
+from common.services.form_services import get_object_form
+from common.services.order_services import filter_order
+
 from . import forms
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class FindOrderViews(FormView):
+    """Поиск заказов"""
+
     form_class = forms.FindOrderForm
     template_name = "find_order_app/find_order.html"
 
     def form_valid(self, form):
         message = "Найденные заказы"
         found = None
-        select = form.cleaned_data["select"]
+        select = get_object_form(form, "select")
+        # поиск по id
         if select == "id":
-            id = form.cleaned_data["order_id_field"]
-            orders = Order.objects.filter(id=id)
+            id = get_object_form(form, "order_id_field")
+            orders = filter_order(id=id)
             if orders:
                 found = orders
             else:
                 message = f"Заказа с номером:{id} не найдено"
+        # поиск по номеру стола
         if select == "table_number":
-            table_number = form.cleaned_data["order_table_number_field"]
-            orders = Order.objects.filter(table_number=table_number)
+            table_number = get_object_form(form, "order_table_number_field")
+            orders = filter_order(table_number=table_number)
             if orders:
                 found = orders
             else:
                 message = f"Заказов за столом {table_number} не найдено"
+        # поиск по статусу
         if select == "status":
-            status = form.cleaned_data["order_status_field"]
-            orders = Order.objects.filter(status=status)
+            status = get_object_form(form, "order_status_field")
+            orders = filter_order(status=status)
             if orders:
                 found = orders
             else:
@@ -41,5 +46,3 @@ class FindOrderViews(FormView):
             message=message,
         )
         return self.render_to_response(context)
-
-
