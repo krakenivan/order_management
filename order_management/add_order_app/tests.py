6a27346c9c73f34627c25bdc8fa60162f687f135
krakenivan <krakenivan777@gmail.com>
2025-03-23 08:47:18 +0400

    test add_order_app

--- order_management/add_order_app/tests.py
+++ order_management/add_order_app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .forms import OrderForm
+
+
+class TestAddOrder(TestCase):
+    def test_creating_order_get(self):
+        response = self.client.get(reverse("creating_order"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context_data['form'], OrderForm)
+
+    def test_creating_order_post(self):
+        data = {
+            "table_number":'1',
+            "product_1":'on',
+            "quantity_1":'1'    
+            }
+        response = self.client.post(reverse("creating_order"),data=data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context_data['form'], OrderForm)
+
+    def test_completing_add_order(self):
+        response = self.client.get("/completing_add_order/")
+        self.assertEqual(response.status_code, 200)
