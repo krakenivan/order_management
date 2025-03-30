0f28d1ff6bf3b554baffe71abf684a7dee3ce912
krakenivan <krakenivan777@gmail.com>
2025-03-30 16:42:23 +0400

    test show_orders_app views

--- order_management/show_orders_app/tests.py
+++ order_management/show_orders_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ShowOrdersViewsTest(TestCase):
+    def test_show_orders_get(self):
+        """тест get запроса"""
+        response = self.client.get(reverse("show_orders"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("orders", response.context)
+        self.assertTemplateUsed(response, "show_orders_app/orders.html")
+
+
+class NoOrdersViewsTest(TestCase):
+    def test_no_orders_get(self):
+        """тест get запроса"""
+        response = self.client.get("/no-orders/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "show_orders_app/no_orders.html")
+
